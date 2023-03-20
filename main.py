9db54b61762f0e3a100db1c51cb5ca39d12cb59a
Manvir Mann <manvirm29@gmail.com>
2023-03-19 21:34:40 -0700

    Extract file and guess password

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-if __name == '__main__':
+import zipfile
+
+def extractFile(zfile, password):
+    try:
+        zfile.extractall(pwd=bytes(password, 'utf-8'))
+        return password
+    except:
+        print('Incorrect pass')
+        return 
+
+def main():
+    zfile = zipfile.ZipFile('test.zip')
+    passFile = open('passlist.txt')
+    for line in passFile.readlines():
+        password = line.strip('\n')
+        guess = extractFile(zfile, password)
+        if guess:
+            print('Password = ' + password)
+            break
+
+if __name__ == '__main__':
     main()
